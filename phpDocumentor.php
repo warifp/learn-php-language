29437260d29ab88a9a168fbd5196fa529a62f552
Wahyu Arif Purnomo <warifp@gmail.com>
2021-10-20 06:22:01 +0700

    add php documentor

---
+++ phpDocumentor.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @category  Hacktoberfest2021
+ * @package   WARIFP
+ * @author    Wahyu Arif Purnomo <hi@warifp.co>
+ * @copyright 2021 WARIFP
+ * @license   MIT License <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/warifp/learn-php-language
+ * @since     20 October 2021
+ */
+
+echo "Hello World";
