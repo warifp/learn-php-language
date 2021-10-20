445491f0b118b3bc893f033d3f6cdfa84a909b2a
Wahyu Arif Purnomo <warifp@gmail.com>
2021-10-20 08:24:51 +0700

    update travis

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Hello World";
+exec('dir /b *.php >> filelist.txt');
+
+$datas = file_get_contents('filelist.txt');
+$datas = explode("\n", str_replace("\r", "", $datas));
+
+foreach ($datas as $key => $data) {
+    if ($data) {
+        if ($data !== 'index.php') {
+            exec('php ' . $data);
+        }
+    }
+}
+
+unlink('filelist.txt');
